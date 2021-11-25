ffcff9262c0be2d367f0d839ca19f106d3451f95
Taehee Lim <lim3617@naver.com>
2021-11-25 21:08:20 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="menu">
+    <a v-for="(a,i) in 메뉴들" :key="i">{{ a }}</a>
+  </div>
+
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+      <button @click="모달창열렸니 = false">닫기</button>
+    </div>
+  </div>
+
+  <div v-for="a in 원룸들" :key="a">
+    <img :src="a.image" class="room-img">
+    <h4>{{ a.title }}</h4>
+    <p>{{ a.price }}원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from './assets/oneroom.js';
+
+
 
 export default {
   name: 'App',
+  data(){
+    return {
+      원룸들 : data,
+      모달창열렸니 : false,
+      신고수 : [0,0,0],
+      메뉴들 : ['Home','Shop','About'],
+      products : ['역삼동원룸','천호동원룸','마포구원룸'],
+    }
+  },
+  methods: {
+    increase(){
+      this.신고수[0] += 1;
+    },
+    increase2(){
+      this.신고수[1] += 1;
+    },
+    increase3(){
+      this.신고수[2] += 1;
+    }
+  },
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
+body {
+  margin : 0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +76,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
